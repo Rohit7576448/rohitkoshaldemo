d617113692af65d52a96ce467652bcdb2ce8f560
Rohit Koushal <rohitkoushal7576448@gmail.com>
2024-10-15 06:09:41 +0000

    dtatypes

---
+++ rohitDemo/demo.js
@@ -0,0 +1,11 @@
+"user strict"; //Treat all JS code as newer version
+ console.log(typeof "Rohit") //String
+ console.log(typeof undefined) //undefined
+
+ //number=>2 to power 53
+ //bigint
+ //string => ""
+ //boolean =>true/false
+ //null => stadlone value
+ //undefined => variable define but value not assinged with variable
+ //symbol => unique
---
+++ rohitDemo/rohit.js
@@ -0,0 +1,12 @@
+const accountId=1441451
+let accountEmail="rohit@gmail.com"
+var accountPassword="123452"
+accountCity="Andra Pradesh"
+//accountId=2
+//console.log(accountId);
+accountEmail="koushal@gamil.com"
+accountPassword="254454646"
+accountCity="Kolkata"
+console.table([accountEmail,accountId,accountPassword,accountCity]);
+console.log(accountCity);  
+      
