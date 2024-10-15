89ab01aac82f957aa73bbb7b44c1e8773e562890
Rohit Koushal <rohitkoushal7576448@gmail.com>
2024-10-15 06:34:44 +0000

    conversion of datatype

---
+++ rohitDemo/Conversion.js
@@ -0,0 +1,23 @@
+let score =null
+//console.log(typeof score);
+///console.log(typeof (score));
+///let neNUmber= Number(score);
+//console.log(typeof neNUmber);
+///console.log(neNUmber);
+
+
+
+// 1 => true; 0 => false
+//"hitesh" => true
+//"" => false
+let isLoggedIn= "rohit"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+
+
+let someNumber= 33
+let striNumber= String(someNumber);
+console.log(striNumber);
+console.log(typeof striNumber);
