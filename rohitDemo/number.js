60788d7a41e1816bf4db8562ba68a81634ee1a3e
Rohit Koushal <rohitkoushal7576448@gmail.com>
2024-10-15 13:17:38 +0000

    numbersolution

---
+++ rohitDemo/number.js
@@ -0,0 +1,6 @@
+const score=5000
+const balance =new Number(4000)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));//it is defined 0 which means convert into decimal number ex-4000.0
+
