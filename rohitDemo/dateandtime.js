ffd72b0b63133db4425cce7adf6c0e02273f6c23
Rohit Koushal <rohitkoushal7576448@gmail.com>
2024-10-16 11:39:45 +0000

    dateand time solve

---
+++ rohitDemo/dateandtime.js
@@ -0,0 +1,13 @@
+//dates
+// let myDate= new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString);
+// console.log(myDate.toLocaleDateString);
+
+// let myCreateDate = new Date(2023,0,23);
+// console.log(myCreateDate.toDateString());
+
+let myTimeStamp =Date.now()
+console.log(myTimeStamp);
+
--- rohitDemo/number.js
+++ rohitDemo/number.js
@@ -1,6 +1,27 @@
-const score=5000
-const balance =new Number(4000)
-console.log(balance);
-console.log(balance.toString().length);
-console.log(balance.toFixed(1));//it is defined 0 which means convert into decimal number ex-4000.0
+//const score=5000
+//console.log(score)
+//const balance =new Number(4000)
+//console.log(balance);
+//console.log(balance.toString().length);//
+//console.log(typeof(balance));
+
+//console.log(balance.toFixed(1));//it is defined 0 which means convert into decimal number ex-4000.0
+
+
+//const hundreds=1000000
+//console.log(hundreds.toLocaleString());//commas standard give answer
+
+// +++++++++++++++++++++++++++++++++++ maths ++++++++++++++++++++++++++++\\
+
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.floor(4.9));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+
+const min =10
+const max=20
+Math.random()
 
