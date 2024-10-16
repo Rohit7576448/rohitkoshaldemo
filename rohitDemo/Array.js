8f7b7aa11d85366c4c471b1b61f8a4f46a408ccc
Rohit Koushal <rohitkoushal7576448@gmail.com>
2024-10-16 12:12:21 +0000

    arraysolve

---
+++ rohitDemo/Array.js
@@ -0,0 +1,46 @@
+//array
+
+const myArr=[0,1,2,3,4,5,6,7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+]
+const myHerose=["rohit","thor"]
+
+const myArr2= new Array(1,2,3,4)
+
+
+
+//Array methods
+
+// myArr.push(6)
+// myArr.push(87)
+// myArr.pop()
+// myArr.unshift(45)
+// console.log(myArr);
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+
+
+
+const myn1=myArr.slice(1,3)
+console.log("A",myArr);
+console.log(myn1);
+
+
+const myn2=myArr.splice(1,3)
+console.log("B",myArr);
+console.log(myn2);
+
