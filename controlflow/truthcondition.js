7a3cb953f630583f00717a0e6663a9d845b75a3c
Rohit Koushal <rohitkoushal7576448@gmail.com>
2024-10-17 18:11:32 +0000

    truthlysolve

---
+++ controlflow/truthcondition.js
@@ -0,0 +1,49 @@
+const userEmail=[]
+// if(userEmail){
+//     console.log("got user email");
+    
+// }
+// else{
+//     console.log("don't have user email");
+    
+// }
+
+//false=> -0,0,BigInt,0n,"",null,undefined,NaN
+
+//truthy values=> "0","false"," ",[],{},function(){}
+
+// if (userEmail.length==0){
+//     console.log("Array is empty");
+    
+// }
+// else{
+//     console.log("Array is not empty");
+    
+// }
+
+// const emptyObj={}
+// if(Object.keys(emptyObj).length===0){
+//     console.log("Object is empty",typeof(emptyObj));//OBJECT access in if condition
+    
+// }
+//Nullish coalescing Operator (??): null undefined
+let val1;
+val1=5 ?? 10//output 5
+
+//val1=null ?? 15
+// val1=undefined??15
+// val1= null?? 10??20
+
+
+
+
+
+//console.log(val1);
+
+
+//Terniary Operator
+
+//condition ? true: false
+const icePrice=100
+icePrice >=80 ? console.log("less than 80"): console.log("more than 80");
+
