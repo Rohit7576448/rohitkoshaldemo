44b4dc6c875f3c315c1f932c5b5515193e2900d5
Rohit Koushal <rohitkoushal7576448@gmail.com>
2024-10-17 19:43:10 +0000

    whileloopsolve

---
+++ controlflow/do_whileandWhile.js
@@ -0,0 +1,24 @@
+//while
+// let index=0
+// while(index<=10){
+//     console.log(`value of index is ${index}`);
+//     index=index+2
+    
+// }
+
+//we can perform same funtion in array
+let Array=["flash","batman","superman"]
+arr=0
+//while
+while(arr< Array.length){
+    console.log(`value is ${Array[arr]}`);
+    arr=arr+1
+    
+}
+//do_while loop
+let score=1
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score<10);
