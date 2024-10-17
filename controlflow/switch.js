50c778eb806e2b23058b93f6e5ec9ec4caa127cd
Rohit Koushal <rohitkoushal7576448@gmail.com>
2024-10-17 17:46:03 +0000

    switchsolve

---
+++ controlflow/switch.js
@@ -0,0 +1,30 @@
+const month= 5
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+        
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+        
+        
+
+
+    default:
+        console.log("Default case match");
+        
+        break;
+}
